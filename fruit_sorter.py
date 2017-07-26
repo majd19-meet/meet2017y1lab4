823f075a48c31c05b7626e0295c18d3f9c354b1f
majd19-meet <majd19@meet.mit.edu>
2017-07-26 17:38:57 +0200

    Stopping here

--- fruit_sorter.py
+++ fruit_sorter.py
@@ -1,1 +1,7 @@
-new_fruit
+new_fruit = input ('What fruit am I sorting?')
+if apples:
+    print ('Bin 1')
+elif oranges:
+    print ('Bin 2')
+else:
+    print ('Error! I do not recognise this fruit!')
---
+++ turtle_party.py
@@ -0,0 +1,77 @@
+#strawbarries = 10
+#is_weekend = False
+
+
+#if is_weekend: #weekend
+ #   print ('If is a weekend')
+#else: #weekday
+  #  print ('It is a weekday')
+
+#if strawberries > 39:
+ #   print ('I have enough strawberries')
+#else:
+ #   print ('I have less than 40 strawberries')
+#else: #weekday
+ #   print ('weekday')
+#if strawberries >39 and strawberries < 61:
+ #   print ('I have enough strawberries')
+#else:
+ #   print ('I have not enough or too many strawberries')
+
+
+
+
+
+
+
+strawberries = 39
+is_weekend = True
+
+if is_weekend:
+    if strawberries > 39:
+        print('fun!')
+    else:
+        print('not fun!')
+else:
+    if strawberries < 61 and  strawberries > 39:
+        print ('fun!')
+    else:
+        print('not fun!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
