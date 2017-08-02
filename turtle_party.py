b1ff7e700757499fa4f9ed2b8cd11d38476e3556
majd19-meet <majd19@meet.mit.edu>
2017-08-02 22:54:02 +0300

    Stopping here

--- turtle_party.py
+++ turtle_party.py
@@ -1,77 +1,18 @@
-#strawbarries = 10
-#is_weekend = False
+strawberries = 50
+is_weekend = False
 
-
-#if is_weekend: #weekend
- #   print ('If is a weekend')
-#else: #weekday
-  #  print ('It is a weekday')
-
-#if strawberries > 39:
- #   print ('I have enough strawberries')
-#else:
- #   print ('I have less than 40 strawberries')
-#else: #weekday
- #   print ('weekday')
-#if strawberries >39 and strawberries < 61:
- #   print ('I have enough strawberries')
-#else:
- #   print ('I have not enough or too many strawberries')
-
-
-
-
-
-
-
-strawberries = 39
-is_weekend = True
-
-if is_weekend:
-    if strawberries > 39:
-        print('fun!')
+if is_weekend == False:
+    if strawberries<39:
+        print('Not Fun!')
+    elif strawberries>61:
+        print('Not Fun!')
     else:
-        print('not fun!')
+        print ('Fun!')
+
 else:
-    if strawberries < 61 and  strawberries > 39:
-        print ('fun!')
+    
+    if strawberries>39:
+        print('Fun!')
     else:
-        print('not fun!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print('Not Fun!')
     
